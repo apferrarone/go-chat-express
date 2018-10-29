eb9b67b08f43851e37fcb978b8f0d668b53756f3
Andrew Ferrarone <andy@indexapp.com>
2018-10-29 13:44:51 -0700

    Removes web templating files and updates app.js

--- app/app.js
+++ app/app.js
@@ -1,8 +1,13 @@
+//                  _
+//  _ __ ___   __ _(_)_ __
+// | '_ ` _ \ / _` | | '_ \
+// | | | | | | (_| | | | | |
+// |_| |_| |_|\__,_|_|_| |_|
+//
+
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
@@ -10,20 +15,16 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// TODO: connect to the db asap:
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// mount routing middleware:
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,13 +35,14 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  console.log(`\n\n\n${err}\n\n\n`);
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    error: {
+      // app.get('env') returns 'development' if NODE_ENV is undefined
+      message: req.app.get('env') === 'development' ? err.message: 'Not Found'
+    }
+  });
 });
 
 module.exports = app;
