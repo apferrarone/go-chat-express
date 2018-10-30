36d25fd6549bb6019cc8ff34be453c32c1ec0f94
Andrew Ferrarone <andy@indexapp.com>
2018-10-29 19:12:39 -0700

    Creates posts.js and posts_controller.js

--- app/models/post.js
+++ app/models/post.js
@@ -8,3 +8,36 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+/**
+* @description The Post data schema
+* user is the id
+*/
+const postSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    content: { type: String, required: true, maxlength: 400 },
+    comment_count: { type: Number, default: 0, required: true },
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true},
+    point: {
+        type: [Number], // [<longitude>, <latitude>]
+        index: '2dsphere'
+    },
+    is_deleted: Boolean
+}, { timestamps: true }); // adds `createdAt` and `updatedAt`
+
+/**
+* @description Easy setter for lat-long minded folks (b/c Mongo does it in reverse).
+* Mongo does geo indexing by long,lat which is really some international standard
+*/
+postSchema.pre('save', function(next) {
+    this.point = [this.longitude, this.latitude];
+    next();
+});
+
+/////////////////////
+//	Exports
+/////////////////////
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
