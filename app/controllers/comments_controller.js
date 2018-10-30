40fd76cbd761dd7d9b0903d575d00440c9a2735e
Andrew Ferrarone <andy@indexapp.com>
2018-10-29 19:16:00 -0700

    Creates comments_controller.js

---
+++ app/controllers/comments_controller.js
@@ -0,0 +1,158 @@
+//                                           _
+//   ___ ___  _ __ ___  _ __ ___   ___ _ __ | |_ ___
+//  / __/ _ \| '_ ` _ \| '_ ` _ \ / _ \ '_ \| __/ __|
+// | (_| (_) | | | | | | | | | | |  __/ | | | |_\__ \
+//  \___\___/|_| |_| |_|_| |_| |_|\___|_| |_|\__|___/
+//
+
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+const debug = require('debug')('app:controller:posts');
+
+/////////////////////
+//	Comment API
+/////////////////////
+
+/** LIST **/
+
+/**
+* @description Fetches comments for a particular post
+*/
+function commentsForPost(req, res) {
+    const postID = req.params.postID;
+
+    // get all comments where parent_post is postID, not deleted:
+    Comment.fromPostID(postID)
+      .exec()
+      .then(function(comments) {
+          res.json({
+              comments: comments
+          });
+      })
+      .catch(function(err) {
+          console.error(err);
+          res.status(400).json({
+              error: {
+                  code: 400,
+                  message: 'Can\'t find the comments'
+              }
+          });
+      });
+}
+
+/** CREATE **/
+
+/**
+* @description Whips up a new comment
+*/
+function createComment(req, res) {
+    const currentUser = req.user;
+    const currentUserID = currentUser._id;
+    const postID = req.params.postID;
+    const content = req.body.content;
+
+    debug(`${currentUser} commenting on post ${postID}`);
+    // to save it, first fetch the post, save the comment to that.
+    // order of ops matters for data integrity,
+    // Fetch the parent post first!!
+    Post.findById(postID)
+      .ne('is_deleted', true)
+      .exec()
+      .then(function(post) {
+          if (post) {
+              // now create the comment:
+              const comment = new Comment({
+                  user: currentUserID,
+                  parent_post: post._id,
+                  content: content
+              });
+              // save the comment, return the promise chain:
+              return comment.save().then(function(comment) {
+                  // increment the comment count for post:
+                  post.comment_count += 1;
+                  // save the post, passing along promise resolved to result:
+                  return post.save().then(function(post) {
+                      // we are done so return comment:
+                      return comment;
+                  });
+              });
+
+          } else {
+              console.log(`\n\n\n\n${err}\n\n\n\n`);
+              // no post found, throw error to be caught below:
+              const err = new Error('Invalid parent post');
+              err.safeMessage = 'The parent post doesn\'t exist';
+              err.code = 400;
+              throw err;
+          }
+      })
+      .then(function(comment) {
+          // success, respond w/ the comment:
+          res.status(201).json(comment);
+      })
+      .catch(function(err) {
+          console.error(err);
+          const code = err.code || 400;
+          res.status(400).json({
+              error: {
+                  code: code,
+                  message: err.safeMessage || 'Could not save the comment'
+              }
+          });
+      });
+}
+
+/** DELETE **/
+
+/**
+* @description Trashes a comment
+*/
+function destroyComment(req, res) {
+    const thisUser = req.user._id;
+    const postID = req.params.postID;
+    const commentID = req.params.commentID;
+
+    // mark the comment as deleted and update it:
+    Comment.findById(commentID)
+      .where('user', thisUser)
+      .update({ is_deleted: true })
+      .exec()
+      .then(function(commentWriteOp) {
+          // if the comment was successfully updated (to soft-delete)
+          if (commentWriteOp.nModified > 0) {
+              // decrement the parent post comment_count, return promise chain:
+              return Post.findById(postID)
+                .update({ $inc: { comment_count: -1} })
+                .exec();
+
+          } else { // something funny is going on...
+              // this probably isn't their post:
+              const err = new Error('Comment delete went off the rails after fetch');
+              throw err;
+          }
+
+      })
+      .then(function(post) {
+          // success:
+          res.json({
+              success: true
+          });
+      })
+      .catch(function(err) {
+          console.error(err);
+          res.status(400).json({
+              error: {
+                  code: err.code || 400,
+                  message: 'couldn\'t complete comment deletion'
+              }
+          });
+      });
+}
+
+/////////////////////
+//	Exports
+/////////////////////
+
+module.exports.commentsForPost = commentsForPost;
+module.exports.createComment = createComment;
+module.exports.destroyComment = destroyComment;
---
+++ app/controllers/posts_controller.js
@@ -0,0 +1,221 @@
+//                  _
+//  _ __   ___  ___| |_ ___
+// | '_ \ / _ \/ __| __/ __|
+// | |_) | (_) \__ \ |_\__ \
+// | .__/ \___/|___/\__|___/
+// |_|
+//
+
+const mongoose = require('mongoose');
+const debug = require('debug')('app:controller:posts');
+
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+
+const EARTH_RADIUS = 3959.0; // miles
+const METERS_PER_MILE = 1609.34;
+
+/////////////////////
+//  Middleware
+/////////////////////
+
+function checkPostID(req, res, next) {
+    const postID = req.params.postID;
+    if (!mongoose.Types.ObjectId.isValid(postID)) {
+        return res.status(400).json({
+            error: {
+                code: 400,
+                message: 'Invalid postID'
+            }
+        });
+    }
+    // postID is valid so continue:
+    next();
+}
+
+/////////////////////
+//	Post API
+/////////////////////
+
+/** CREATE **/
+
+/**
+* @description Create a new post
+*/
+function createPost(req, res) {
+    const user = req.user;
+    const content = req.body.content;
+    const latitude = req.body.latitude || req.body.lat;
+    const longitude = req.body.longitude || req.body.long;
+
+    debug(`User ${user._id} creating post at ${latitude}, ${longitude}`);
+
+    const post = new Post({
+        user: user._id,
+        content: content,
+        latitude: latitude,
+        longitude: longitude
+    });
+
+    post.save()
+      .then(function(post) {
+          res.status(201).json(post);
+      })
+      .catch(function(err) {
+          res.status(400).json({
+              error: {
+                  code: err.code || 400,
+                  message: err.message
+              }
+          });
+      });
+}
+
+/** READ **/
+
+/**
+* @description Finds a post by post ID
+*/
+function findPost(req, res) {
+    const postID = req.params.postID;
+
+    // find the post by id:
+    Post.findById(postID)
+      .exec()
+      .then(function(post) {
+          if (post) {
+              res.json(post);
+          } else {
+              // no post found, throw error to be caught below:
+              const err = new Error('Invalid post id');
+              err.safeMessage = 'That post doesn\'t exist';
+              err.code = 400;
+              throw err;
+          }
+      })
+      .catch(function(err) {
+          console.error(err);
+          res.status(400).json({
+              error: {
+                  code: err.code || 400,
+                  message: err.safeMessage || 'Error when looking for post'
+              }
+          });
+      });
+}
+
+/** DELETE **/
+
+/**
+* @description Marks a post as deleted (soft delete)
+*/
+function destroyPost(req, res) {
+    const thisUser = req.user._id;
+    const postID = req.params.postID;
+
+    debug(`user ${thisUser} deleting post ${postID}`);
+
+    Post.findById(postID)
+      .where('user', thisUser)
+      .update({ is_deleted: true })
+      .exec()
+      .then(function(modified) {
+
+          // for query.prototype.update, modified is writeOpResult
+          if (modified.nModified > 0) {
+              // success:
+              res.json({
+                  success: true
+              });
+
+          } else { // post may not belong to thisUser, funny business here...
+              debug(`User ${thisUser} might not own post ${postID}`);
+              res.status(401).json({
+                  error: {
+                      code: 401,
+                      message: 'Something fishy is going on.'
+                  }
+              });
+          }
+      })
+      .catch(function(err){
+          console.error(err);
+          res.status(500).json({
+              error: {
+                  code: err.code || 500,
+                  message: 'Couldn\'t delete the post'
+              }
+          });
+      });
+}
+
+/** SEARCH **/
+
+/**
+* @description Searches for local posts by lat/long
+*/
+function postsByLocation(req, res) {
+    const user = req.user;
+    const lat = req.query.latitude || req.query.lat;
+    const long = req.query.longitude || req.query.long;
+    const radiusMiles = parseFloat(req.query.within) || 5.0;
+
+    queryPostsAtLocation(user, lat, long, radiusMiles)
+    .then(function(posts) {
+        res.json({
+            posts: posts
+        });
+    })
+    .catch(function(err) {
+        console.log(`\n\n${'HERE'}\n\n`);
+        console.log(err);
+        res.status(500).json({
+            error: {
+                code: error.code || 500,
+                message: error.message
+            }
+        });
+    });
+}
+
+/**
+* @description Utility for querying posts and autoexpanding radius !
+*/
+function queryPostsAtLocation(user, lat, long, radiusMiles) {
+    var radiusRadians = radiusMiles / EARTH_RADIUS;
+    // use x/y coordinates for Mongo;
+    var xyCoordinate = [long, lat];
+    // construct a geoJSON point:
+    var point = {
+        type: 'Point',
+        center: xyCoordinate,
+        maxDistance: radiusRadians,
+        spherical: true
+    };
+
+    debug(`${user._id} Looking for posts ${radiusMiles} miles and ${radiusRadians} radians from ${xyCoordinate}`);
+
+    // look for public posts w/in X miles, not deleted, by most recent:
+    var queryPromise = Post.find()
+      .ne('is_deleted', true)
+      .near('point', point)
+      .sort('-createdAt')
+      .limit(200)
+      .exec()
+      .then(function(posts) {
+          debug(`${posts.length} posts found.`);
+          return posts;
+      });
+
+      return queryPromise;
+}
+
+/////////////////////
+//	Exports
+/////////////////////
+
+module.exports.checkPostID = checkPostID;
+module.exports.createPost = createPost;
+module.exports.findPost = findPost;
+module.exports.destroyPost = destroyPost;
+module.exports.postsByLocation = postsByLocation;
